79f9613fdbc2ad1827160a03630e352c0a87b281
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-18 00:34:37 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jiyulBaek/Login/Login.js
+++ src/pages/jiyulBaek/Login/Login.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import './Login.scss';
 
 function LoginJiyul() {
   const navigate = useNavigate();
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+
+  const saveUserId = e => {
+    setUserId(e.target.value);
+  };
+
+  const saveUserPw = e => {
+    setUserPw(e.target.value);
+  };
+
   return (
     <div className="login">
       <p className="title">westagram</p>
@@ -11,8 +22,9 @@ function LoginJiyul() {
         className="id-input"
         type="text"
         placeholder="전화번호, 사용자 이름 또는 이메일"
+        onChange={saveUserId}
       />
-      <input type="password" placeholder="비밀번호" />
+      <input type="password" placeholder="비밀번호" onChange={saveUserPw} />
       <Link to="/main">
         <button
           onClick={() => {
