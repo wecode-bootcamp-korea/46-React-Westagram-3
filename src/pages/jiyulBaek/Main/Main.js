b0341201d920f82bff1d6b5adfda94369b5fb64f
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-23 01:02:09 +0900

    Assignment 2 | Mock Data를 활용하여 여러 개의 피드 구현

--- src/pages/jiyulBaek/Main/Main.js
+++ src/pages/jiyulBaek/Main/Main.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Comment from './Comment';
-import { FOOTER_INFO_LIST } from './data.js';
+import { FOOTER_INFO_LIST } from '../data/data.js';
 import './Main.scss';
 
 function MainJiyul() {
   const [inputValue, setInputValue] = useState('');
   const [comments, setComments] = useState([{ id: 1, comment: '' }]);
+  const [feedList, setFeedList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/feedData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setFeedList(data);
+      });
+  }, []);
 
   const addComment = () => {
     if (inputValue !== '') {
@@ -101,90 +112,96 @@ function MainJiyul() {
 
       <main>
         <section className="feeds">
-          <div className="top">
-            <div className="top-logo">
-              <img
-                src="https://images.unsplash.com/photo-1507166763745-bfe008fbb831?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="accountImage1"
-              />
-              <span>jy_baek</span>
-            </div>
-            <div className="top-icon">
-              <img
-                className="fa-ellipsis"
-                src="/images/jiyulBaek/ellipsis.png"
-                alt="ellipsis"
-              />
-            </div>
-          </div>
+          {feedList.map(feed => {
+            return (
+              <div className="feed" key={feed.id}>
+                <div className="top">
+                  <div className="top-logo">
+                    <img
+                      src={`${feed.account_url}`}
+                      alt={`accountImage${feed.id}`}
+                    />
+                    <span>{feed.username}</span>
+                  </div>
+                  <div className="top-icon">
+                    <img
+                      className="fa-ellipsis"
+                      src="/images/jiyulBaek/ellipsis.png"
+                      alt="ellipsis"
+                    />
+                  </div>
+                </div>
 
-          <div className="image">
-            <img src="/images/jiyulBaek/feed.jpg" alt="feedImage" />
-          </div>
+                <div className="image">
+                  <img src={`${feed.feed_url}`} alt="feedImage" />
+                </div>
 
-          <div className="feed-icons">
-            <div className="feed-icons-left">
-              <img
-                className="fa-heart"
-                src="/images/jiyulBaek/heart_white.png"
-                alt="unclicked_heart"
-              />
-              <img
-                className="fa-comment"
-                src="/images/jiyulBaek/comment.png"
-                alt="comment-icon"
-              />
-              <img
-                className="fa-arrow-up-from-bracket"
-                src="/images/jiyulBaek/ArrowUpFromBracket.png"
-                alt="share-icon"
-              />
-            </div>
-            <div className="feed-icons-right">
-              <img
-                className="fa-bookmark"
-                src="/images/jiyulBaek/bookmark.png"
-                alt="bookmark-icon"
-              />
-            </div>
-          </div>
+                <div className="feed-icons">
+                  <div className="feed-icons-left">
+                    <img
+                      className="fa-heart"
+                      src="/images/jiyulBaek/heart_white.png"
+                      alt="unclicked_heart"
+                    />
+                    <img
+                      className="fa-comment"
+                      src="/images/jiyulBaek/comment.png"
+                      alt="comment-icon"
+                    />
+                    <img
+                      className="fa-arrow-up-from-bracket"
+                      src="/images/jiyulBaek/ArrowUpFromBracket.png"
+                      alt="share-icon"
+                    />
+                  </div>
+                  <div className="feed-icons-right">
+                    <img
+                      className="fa-bookmark"
+                      src="/images/jiyulBaek/bookmark.png"
+                      alt="bookmark-icon"
+                    />
+                  </div>
+                </div>
 
-          <div className="likes">
-            <img
-              src="https://images.unsplash.com/photo-1587161584760-f51779fb276a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              alt="accountImage2"
-            />
-            <span>
-              <span className="likesInfo">aineworld</span>님 외
-              <span className="likesInfo">10명</span>이 좋아합니다.
-            </span>
-          </div>
+                <div className="likes">
+                  <img
+                    src="https://images.unsplash.com/photo-1587161584760-f51779fb276a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="accountImage2"
+                  />
+                  <span>
+                    <span className="likesInfo">aineworld</span>님 외
+                    <span className="likesInfo">10명</span>이 좋아합니다.
+                  </span>
+                </div>
 
-          <div className="contents">
-            <span className="contents-account">jy_baek</span>
-            <span>위워크에서 진행된 베이킹 클래스...</span>
-            <span>더 보기</span>
-            <div>42분 전</div>
-          </div>
+                <div className="contents">
+                  <span className="contents-account">{feed.username}</span>
+                  <span>{feed.company.catchPhrase}</span>
+                  <span>더 보기</span>
+                  <div>42분 전</div>
+                </div>
 
-          <ul className="comment-list">
-            {comments.map(comment => (
-              <Comment key={comment.id} comment={comment.comment} />
-            ))}
-          </ul>
+                <ul className="comment-list">
+                  {comments.map(comment => (
+                    <Comment key={comment.id} comment={comment.comment} />
+                  ))}
+                </ul>
 
-          <div className="comment-input">
-            <input
-              value={inputValue}
-              type="text"
-              onChange={event => setInputValue(event.target.value)}
-              onKeyUp={handleKeyUp}
-              placeholder="댓글 달기"
-            />
-            <button onClick={addComment}>게시</button>
-          </div>
+                <div className="comment-input">
+                  <input
+                    key={feed.id}
+                    value={inputValue}
+                    type="text"
+                    onChange={event => setInputValue(event.target.value)}
+                    onKeyUp={handleKeyUp}
+                    placeholder="댓글 달기"
+                  />
+                  <button onClick={addComment}>게시</button>
+                </div>
+              </div>
+            );
+          })}
         </section>
-
         <section className="main-right">
           <div className="account">
             <img
