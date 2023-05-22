918ebbc5f0fbebf190e2a947aef047275f3877d7
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-22 20:21:06 +0900

    Assignment1: Aside에서 상수 데이터 활용 - 1-3. 파일 분리

--- src/pages/jiyulBaek/Main/Main.js
+++ src/pages/jiyulBaek/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Comment from './Comment';
+import { FOOTER_INFO_LIST } from './data.js';
 import './Main.scss';
 
 function MainJiyul() {
@@ -287,16 +288,3 @@ function MainJiyul() {
 }
 
 export default MainJiyul;
-
-const FOOTER_INFO_LIST = [
-  { id: 0, span: 'Instagram 정보' },
-  { id: 1, span: '지원 ·' },
-  { id: 2, span: '홍보센터 ·' },
-  { id: 3, span: 'API ·' },
-  { id: 4, span: '채용정보 ·' },
-  { id: 5, span: '약관 ·' },
-  { id: 6, span: '디렉터리 ·' },
-  { id: 7, span: '프로필 ·' },
-  { id: 8, span: '해시태그 ·' },
-  { id: 9, span: '언어' },
-];
---
+++ src/pages/jiyulBaek/Main/data.js
@@ -0,0 +1,12 @@
+export const FOOTER_INFO_LIST = [
+  { id: 0, span: 'Instagram 정보 ·' },
+  { id: 1, span: '지원 ·' },
+  { id: 2, span: '홍보센터 ·' },
+  { id: 3, span: 'API ·' },
+  { id: 4, span: '채용정보 ·' },
+  { id: 5, span: '약관 ·' },
+  { id: 6, span: '디렉터리 ·' },
+  { id: 7, span: '프로필 ·' },
+  { id: 8, span: '해시태그 ·' },
+  { id: 9, span: '언어' },
+];
